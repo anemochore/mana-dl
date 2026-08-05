8dfc6a671ebd63ca3269f1e71a413d27306d4492
anemochore <fallensky@naver.com>
2026-08-05 11:00:26 +0900

    fix check logic for 11toon

--- util/common.js
+++ util/common.js
@@ -70,7 +70,7 @@ function initSettingContainer(container, epNumber, main) {
     }
     else if (target == input1 || target == input2 || target.classList.contains('fy-check')) {
       if (target == input1 || target == input2) {
-        //범위 선택
+        //범위 입력 시
         let [value1, value2] = [parseInt(input1.value), parseInt(input2.value)];
         if (value1 > value2) {
           [input1.value, input2.value] = [input2.value, input1.value];
@@ -78,30 +78,36 @@ function initSettingContainer(container, epNumber, main) {
         }
         items.forEach(item => item.checked = false);
         items.toReversed().slice(value1-1, value2).forEach(item => item.checked = true);
-        checkedCount = items.filter(i => i.checked).length;
       }
       else {
-        //목록에서 선택
+        //목록의 체크박스 클릭 시
         const [value1, value2] = [parseInt(input1.value), parseInt(input2.value)];
 
         //배열 trim
-        //console.debug('items',items.map(el => el.nextSibling.textContent));
         const start = items.findIndex(v => v.checked);
         const end = items.length - items.toReversed().findIndex(v => v.checked);
         const trimmed = items.slice(start, end);
-        //console.debug('trimmed',trimmed.map(el => el.nextSibling.textContent));
-        if (trimmed.some(v => !v.checked)) {
+        if (!trimmed.every(v => v.checked)) {
           //중간이 비어 있다면 범위 선택 X
           input1.disabled = input2.disabled = true;
         }
         else {
           input1.disabled = input2.disabled = false;
-          input1.value = parseInt(trimmed.at(-1).nextSibling.textContent);
-          input2.value = parseInt(trimmed[0].nextSibling.textContent);
+          const value1 = parseInt(trimmed.at(-1).nextSibling.textContent);
+          const value2 = parseInt(trimmed[0].nextSibling.textContent);
+
+          //일일툰은 목록에 회차 텍스트가 없음
+          if (isNaN(value1)) input1.value = start + 1
+          else input1.value = value1;
+          if (isNaN(value2)) input2.value = end;
+          else input2.value = value2;
+          //console.debug('start, end, input1.value, input2.value, trimmed', start, end, input1.value, input2.value, trimmed);
         }
       }
 
       //indeterminate visual
+      checkedCount = items.filter(i => i.checked).length;
+      console.debug('checkedCount, items.length', checkedCount, items.length);
       if (checkedCount == 0) {
         selectAll.checked = false;
         selectAll.indeterminate = false;
@@ -142,65 +148,141 @@ function zipAndDownload(zip, fileName, console2 = console) {
 }
 
 class FadingAlert {
-  constructor(backgroundColor = 'pink') {
-    this.div = document.createElement('div');
-    this.div.id = 'alertBoxDiv';
-    document.body.appendChild(this.div);
+  constructor(styleObj = {}) {
+    this.div = document.getElementById('alertBoxDiv');
+
+    if (!this.div) {
+      this.div = document.createElement('div');
+      this.div.id = 'alertBoxDiv';
+
+      this.div.style.opacity = 0;
+      this.div.style.pointerEvents = 'none';
+      this.div.style.transition = 'none';
+
+      document.body.appendChild(this.div);
+    }
+
+    this.textEl = this.div.querySelector('.fading-alert-text');
+    if (!this.textEl) {
+      this.textEl = document.createElement('span');
+      this.textEl.className = 'fading-alert-text';
+      this.div.appendChild(this.textEl);
+    }
 
     const s = this.div.style;
-    s.position = 'fixed';
-    s.top = '40%'; s.left = '45%';
-    s.width = '240px'; s.height = 'auto';
-    s.textAlign = 'center'; s.padding = '2px';
-    s.color = 'Black'; s.backgroundColor = backgroundColor;
-    s.border = 0; s.overflow = 'auto';
+    Object.assign(s, styleObj);
 
-    this.log_ = (func = console.log, ...txt) => {
-      if (this.interval) {
-        this.interval = null;
-        clearInterval(this.interval);
+    s.position ||= 'fixed';
+    s.top ||= '25%';
+    if (!s.left) {
+      s.left = '50%';
+      s.transform ||= 'translateX(-50%)';
+    }
+    s.width ||= '250px';
+    s.textAlign ||= 'center';
+    s.padding ||= '2px';
+    s.color ||= 'black';
+    s.backgroundColor ||= 'pink';
+    s.border ||= '0';
+    s.overflow = 'hidden';
+    s.wordBreak ||= 'break-word';
+    s.whiteSpace ||= 'pre-line';
 
-        const spinnerEl = this.div.querySelector('span');
-        if (spinnerEl) spinnerEl.textContent = '';
-      }
+    this.div.addEventListener('click', () => {
+      this.fadeOut();
+    });
+  }
 
-      if (txt.length == 0 || !txt[0]) {
-        this.div.style.opacity = 0;
-        this.div.style.transition = 'opacity 5s ease-in';
-      }
-      else {
-        this.div.style.transition = '';
-        this.div.style.opacity = 1;
-        this.div.textContent = txt.join(' ');
-        func(...txt);
-      }
+  log_(func = console.log, ...txt) {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+
+    if (this.spinnerEl) {
+      this.spinnerEl.textContent = '';
+    }
+
+    if (txt.length === 0 || !txt[0]) {
+      if (this.div.style.opacity == 1) this.fadeOut();
+    }
+    else {
+      this.textEl.textContent = txt.join(txt[0].endsWith('\n') ? '' : ' ');
+      this.div.style.transition = '';
+      this.div.style.opacity = 1;
+      this.div.style.pointerEvents = 'auto';
+      func(...txt);
+    }
+  }
+
+  log(...txt) {
+    this.log_(console.log, ...txt);
+  }
+
+  add(...txt) {
+    if (this?.spinnerEl.textContent) this.log_(console.log, ...txt);  //ignore text of previous spin()
+    else this.log_(console.log, this.textEl.textContent + '\n\n', ...txt);
+  }
+
+  get() {
+    return this.textEl.textContent;
+  }
+
+  show(...txt) {
+    this.log_(() => {},    ...txt);
+  }
+
+  spin(...txt) {
+    this.log_(console.log, ...txt);
+
+    this.spinnerEl ||= this.div.querySelector('.fading-alert-spinner');
+    if (!this.spinnerEl) {
+      this.spinnerEl = document.createElement('span');
+      this.spinnerEl.className = 'fading-alert-spinner';
+      this.spinnerEl.style.fontFamily = "'Courier New', monospace";
+      this.spinnerEl.style.marginLeft = '8px';
+      this.div.appendChild(this.spinnerEl);
+    }
+
+    const spinner = ['|', '/', '-', '\\'];
+    let i = 0;
+
+    this.spinnerEl.textContent = spinner[0];
+
+    if (this.interval) clearInterval(this.interval);
+    this.interval = setInterval(() => {
+      this.spinnerEl.textContent = spinner[i++ % spinner.length];
+    }, 100);
+  }
+
+  fadeOut() {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+    if (this.spinnerEl) {
+      this.spinnerEl.textContent = '';
+    }
+
+    this.div.style.transition = '';
+    this.div.style.opacity = 1;
+
+    void this.div.offsetWidth;  //강제 reflow
+
+    this.div.style.transition = 'opacity 3s ease-in';
+    this.div.style.opacity = 0;
+    this.div.style.pointerEvents = 'none';
+
+    const onEnd = (e) => {
+      this.textEl.textContent = '';
+      this.div.style.transition = '';
     };
-    this.log = (...txt) => this.log_(console.log, ...txt);
-    this.show = (...txt) => this.log_(() => { }, ...txt);
 
-    this.spin = (...txt) => {
-      this.log_(console.log, ...txt);
-
-      let spinnerEl = this.div.querySelector('span');
-      if (!spinnerEl) {
-        spinnerEl = document.createElement('span');
-        spinnerEl.style.fontFamily = "'Courier New', monospace";
-        spinnerEl.style.marginLeft = '8px';
-        this.div.appendChild(spinnerEl);
-      }
-
-      spinnerEl.textContent = '|';
-      const spinner = ['|', '/', '-', '\\'];
-      let i = 0;
-
-      this.interval = setInterval(() => {
-        spinnerEl.textContent = spinner[i++ % spinner.length];
-      }, 100);
-    };
-
-    this.log();
+    this.div.addEventListener('transitionend', onEnd, { once: true });
   }
 }
+
+//globalThis.FadingAlert = FadingAlert;
 
 function sleep(ms) {
   return new Promise(r => setTimeout(r, ms));
